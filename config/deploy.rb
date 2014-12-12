b46446822198bee862c60795b2078751c73ae31c
sanemat <o.gata.ken@gmail.com>
2014-12-13 00:23:44 +0900

    Use link_dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,9 @@ set :repo_url, ENV['CAP_REPO_URL']
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs,
+    fetch(:linked_dirs, [])
+  .push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
