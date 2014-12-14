77927f5cae7b6b9de37155a173dd7920b1a299ee
sanemat <o.gata.ken@gmail.com>
2014-12-14 13:33:07 +0900

    Fix rubocop

--- test/controllers/v1/files_controller_test.rb
+++ test/controllers/v1/files_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class V1::FilesControllerTest < ActionController::TestCase
-  test "should get show" do
+  test 'should get show' do
     get :show
     assert_response :success
   end
-
 end
