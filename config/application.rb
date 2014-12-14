a128cbdba57d8170437c289baf227f87d8f53d95
sanemat <o.gata.ken@gmail.com>
2014-12-14 20:00:47 +0900

    Adjust cors

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,12 @@ module ForestWildcat
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.middleware.use Rack::Health
+
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
